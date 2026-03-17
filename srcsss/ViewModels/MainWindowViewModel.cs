80c13ef27e9c7176ad8c7834369e4c5d3a945f82
Hofer Lukas <lukas.hofer.ju@gmail.com>
2026-03-18 00:08:02 +0100

    Suppression du dossier crée par inadvertance

--- srcsss/ViewModels/MainWindowViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace ScholarLog.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
-{
-    public string Greeting { get; } = "Welcome to Avalonia!";
-}
--- srcsss/ViewModels/ViewModelBase.cs
+++
@@ -1,7 +0,0 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
-
-namespace ScholarLog.ViewModels;
-
-public abstract class ViewModelBase : ObservableObject
-{
-}
