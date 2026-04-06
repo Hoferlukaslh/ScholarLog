b9e6f71367f859eb27da5f8d49c6ca5f9be97834
Hofer Lukas <lukas.hofer.ju@gmail.com>
2026-04-06 22:59:54 +0200

    Ajout log trace si DEBUG

--- src/Program.cs
+++ src/Program.cs
@@ -14,16 +14,18 @@ sealed class Program
         var builder = AppBuilder.Configure<App>()
             .UsePlatformDetect()
             
+            // Gestion stricte du moteur de rendu (Skia) pour la RAM / VRAM
             .With(new SkiaOptions
             {
+                // Limite le cache GPU (VRAM) à 256 Mo. Évite l'étouffement des machines modestes.
                 MaxGpuResourceSizeBytes = 256 * 1024 * 1024
             });
 
-        // Les logs ne sont compilés et exécutés qu'en environnement de développement
-        // builder.LogToTrace(Avalonia.Logging.LogEventLevel.Warning);
-
+            // Les logs coûtent de la RAM (allocation de chaînes). À exclure en production.
+#if DEBUG
+            builder.LogToTrace(Avalonia.Logging.LogEventLevel.Warning);
+#endif
 
         return builder;
     }
-
 }
